559bb5ab20e0a929d5bc30e5fb6548182a025023
kokovtbg <kokovtbg@gmail.com>
2023-03-25 23:25:48 +0200

    added jwt

---
+++ src/main/java/bg/softuni/pochivkarest/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package bg.softuni.pochivkarest.security.services;
+
+import bg.softuni.pochivkarest.model.entity.User;
+import bg.softuni.pochivkarest.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
