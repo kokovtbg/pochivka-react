bd14a55b8eede5977f000cb14eac8ff9077bd65c
kokovtbg <kokovtbg@gmail.com>
2023-04-04 21:18:35 +0300

    register

--- src/main/java/bg/softuni/pochivkarest/web/rest/AuthController.java
+++ src/main/java/bg/softuni/pochivkarest/web/rest/AuthController.java
@@ -1,8 +1,11 @@
 package bg.softuni.pochivkarest.web.rest;
 
 import bg.softuni.pochivkarest.model.dto.UserLoginDTO;
+import bg.softuni.pochivkarest.model.dto.UserRegisterDTO;
 import bg.softuni.pochivkarest.security.dto.AuthenticationResponse;
 import bg.softuni.pochivkarest.security.services.AuthenticationService;
+import bg.softuni.pochivkarest.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +17,12 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthenticationService authenticationService;
+    private final UserService userService;
 
     @Autowired
-    public AuthController(AuthenticationService authenticationService) {
+    public AuthController(AuthenticationService authenticationService, UserService userService) {
         this.authenticationService = authenticationService;
+        this.userService = userService;
     }
 
     @PostMapping(value = "/login",
@@ -27,5 +32,10 @@ public class AuthController {
             @RequestBody UserLoginDTO request
     ) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/register-rest")
+    public UserRegisterDTO register(@Valid @RequestBody UserRegisterDTO userRegisterDTO) {
+        return userService.register(userRegisterDTO);
     }
 }
