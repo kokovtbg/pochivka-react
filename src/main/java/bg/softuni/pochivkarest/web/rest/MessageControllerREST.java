07ef9c5184ca26469e8f2651b8a9c5956faf85d2
kokovtbg <kokovtbg@gmail.com>
2023-04-06 23:59:39 +0300

    mesages

--- src/main/java/bg/softuni/pochivkarest/web/rest/MessageControllerREST.java
+++ src/main/java/bg/softuni/pochivkarest/web/rest/MessageControllerREST.java
@@ -3,10 +3,9 @@ package bg.softuni.pochivkarest.web.rest;
 import bg.softuni.pochivkarest.model.dto.MessageDTO;
 import bg.softuni.pochivkarest.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
@@ -21,5 +20,15 @@ public class MessageControllerREST {
     @PostMapping("/send-message")
     public MessageDTO sendMessage(@RequestBody MessageDTO messageDTO) {
         return messageService.sendMessage(messageDTO);
+    }
+
+    @GetMapping("/messages-input-rest/{username}")
+    public List<MessageDTO> getAllInputMessages(@PathVariable String username) {
+        return messageService.getAllReceivedMessages(username);
+    }
+
+    @GetMapping("/message-read-rest/{id}")
+    public MessageDTO getMessage(@PathVariable long id) {
+        return messageService.getMessageById(id);
     }
 }
